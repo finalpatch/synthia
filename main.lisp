307cf2aa1b0867953b8dba3adb453edc923ef044
finalpatch <fengli@gmail.com>
2020-02-08 22:13:34 +1100

    refactor

--- main.lisp
+++ main.lisp
@@ -30,46 +30,37 @@
       (al:buffer-data buffer :mono16 device-array
                       (* size sample-width) *sample-rate*))))
 
-(defun samples-duration (data)
-  (/ (length data) *sample-rate*))
+;; -------------------------------------------------------------
 
-(defun play-samples-in-al-context (data)
-  (al:with-source (source)
-    (al:with-buffer (buffer)
-      (fill-al-buffer buffer data)
-      (al:source source :buffer buffer)
-      (al:source-play source)
-      (loop until (equal (al:get-source source :source-state) :stopped)
-            do (sleep 0.01)))))
+(defun osc-zero (freq pos)
+  (declare (ignore freq pos))
+  0)
+
+(defun osc-random (freq pos)
+  (declare (ignore freq pos))
+  (1- (random 2.0)))
+
+(defun osc-sine (freq pos)
+  (let ((time (/ pos *sample-rate*)))
+    (sin (* freq 2 pi time))))
+
+(defun osc-square (freq pos)
+  (if (> (osc-sine freq pos) 0)
+      1.0 -1.0))
 
 ;; -------------------------------------------------------------
+
+(defun gen-samples (synth freq samples &optional (pos 0))
+  (let ((sample-array (make-array samples)))
+    (loop for i from 0 to (1- samples)
+          do (setf (aref sample-array i) (funcall synth freq (+ pos i))))
+    sample-array))
 
 (defparameter *music-scale*
   #(:C  :C#  :D  :D#  :E  :F  :F#  :G  :G#  :A  :A#  :B
     :C2 :C2# :D2 :D2# :E2 :F2 :F2# :G2 :G2# :A2 :A2# :B2))
 
 (defconstant twelveth-root-of-2 (expt 2 (/ 1 12)))
-
-(defun osc-random (pos)
-  (declare (ignore pos))
-  (1- (random 2.0)))
-
-(defun osc-sine (freq)
-  (lambda (pos)
-    (let ((time (/ pos *sample-rate*)))
-      (sin (* freq 2 pi time)))))
-
-(defun osc-square (freq)
-  (let ((sine-generator (osc-sine freq)))
-    (lambda (pos)
-      (if (> (funcall sine-generator pos) 0)
-          1.0 -1.0))))
-
-(defun gen-samples (osc samples &optional (pos 0))
-  (let ((sample-array (make-array samples)))
-    (loop for i from 0 to (1- samples)
-          do (setf (aref sample-array i) (funcall osc (+ pos i))))
-    sample-array))
 
 (defun note-to-freq (note)
   (if (equal note :R)
@@ -78,28 +69,12 @@
                   (position :A *music-scale*))))
         (* 440 (expt twelveth-root-of-2 k)))))
 
-(defun gen-note (name duration &optional (osc 'osc-sine))
-  (gen-samples (funcall osc (note-to-freq name))
-               (floor (+ 0.5 (* duration *sample-rate*)))))
-
 ;; -------------------------------------------------------------
 
-(defun play-samples (data)
-  (with-al-context
-      (play-samples-in-al-context data)))
-
-(defun play-sequence (seq)
-  (with-al-context
-      (loop for e in seq
-            do (play-samples-in-al-context
-                (gen-note (car e) (cadr e))))))
-
-;; -------------------------------------------------------------
-
-(defun stream-buffers (freq pos source buffers)
+(defun stream-buffers (synth freq pos source buffers)
   ;; fill buffers with new samples
   (loop for b in buffers
-        do (let ((samples (gen-samples (osc-square freq) *buffer-samples* pos)))
+        do (let ((samples (gen-samples synth freq *buffer-samples* pos)))
              (fill-al-buffer b samples)
              (incf pos *buffer-samples*)))
   ;; queue buffers on the source
@@ -109,7 +84,25 @@
     (al:source-play source))
   pos)
 
-(defun keyboard ()
+(defun scancode-to-note (scancode)
+  (cond
+    ((sdl2:scancode= scancode :scancode-z) :C)
+    ((sdl2:scancode= scancode :scancode-x) :D)
+    ((sdl2:scancode= scancode :scancode-c) :E)
+    ((sdl2:scancode= scancode :scancode-v) :F)
+    ((sdl2:scancode= scancode :scancode-b) :G)
+    ((sdl2:scancode= scancode :scancode-n) :A)
+    ((sdl2:scancode= scancode :scancode-m) :B)
+    ((sdl2:scancode= scancode :scancode-q) :C2)
+    ((sdl2:scancode= scancode :scancode-w) :D2)
+    ((sdl2:scancode= scancode :scancode-e) :E2)
+    ((sdl2:scancode= scancode :scancode-r) :F2)
+    ((sdl2:scancode= scancode :scancode-t) :G2)
+    ((sdl2:scancode= scancode :scancode-y) :A2)
+    ((sdl2:scancode= scancode :scancode-u) :B2)
+    (t :R)))
+
+(defun keyboard (&optional (synth 'osc-square))
   (sdl2:with-init (:everything)
     (sdl2:with-window (win :flags '(:shown))
       (with-al-context
@@ -117,20 +110,17 @@
           (al:with-buffers (*buffer-count* buffers)
             (let ((freq 0) (pos 0))
               ;; start playing
-              (setf pos (stream-buffers freq pos source buffers))
+              (setf pos (stream-buffers synth freq pos source buffers))
               (format t "Beginning main loop.~%") (finish-output)
               (sdl2:with-event-loop  (:method :poll)
                 (:keydown (:keysym keysym)
-                          (let ((scancode (sdl2:scancode-value keysym))
-                                (sym (sdl2:sym-value keysym))
-                                (mod-value (sdl2:mod-value keysym)))
-                            (cond
-                              ((sdl2:scancode= scancode :scancode-w) (setf freq 440))
-                              )
-                            (format t "Key sym: ~a, code: ~a, mod: ~a~%"
-                                    sym scancode mod-value)))
+                          (let ((scancode (sdl2:scancode-value keysym)))
+                            (setf freq (note-to-freq (scancode-to-note scancode)))))
                 (:keyup (:keysym keysym)
-                        (setf freq 0)
+                        (let ((scancode (sdl2:scancode-value keysym)))
+                          ;; if equal to last pressed key, set to silence
+                          (when (= freq (note-to-freq (scancode-to-note scancode)))
+                            (setf freq 0)))
                         (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
                           (sdl2:push-event :quit)))
                 (:idle ()
@@ -138,33 +128,7 @@
                          (when (> processed 0)
                            (let ((free-buffers (al:source-unqueue-buffers source processed)))
                              ;; keep playing
-                             (setf pos (stream-buffers freq pos source free-buffers))
+                             (setf pos (stream-buffers synth freq pos source free-buffers))
                              (format t "queue buffers ~a~%" processed) (finish-output)))))
                 (:quit () t))
               (format t "Exiting main loop.~%") (finish-output))))))))
-
-;; -------------------------------------------------------------
-
-;; (play-sequence '((:C 0.5)
-;;                  (:C 0.5)
-;;                  (:G 0.5)
-;;                  (:G 0.5)
-;;                  (:A 0.5)
-;;                  (:A 0.5)
-;;                  (:G 0.5)
-;;                  (:R 0.5)
-;;                  (:F 0.5)
-;;                  (:F 0.5)
-;;                  (:E 0.5)
-;;                  (:E 0.5)
-;;                  (:D 0.5)
-;;                  (:D 0.5)
-;;                  (:C 0.5)
-;;                  ))
-
-;; white noise
-;; (play-samples (gen-samples #'osc-random *sample-rate*))
-;; 440hz
-;; (play-samples (gen-samples (osc-square 220) *sample-rate*))
-;; note A
-;; (play-samples (gen-note :A (/ 1 2) 'osc-sine))

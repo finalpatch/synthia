d7e069f350c6238e614a90e56433c74956c4394b
finalpatch <fengli@gmail.com>
2020-02-23 21:05:14 +1100

    2 tracks

--- src/midi.lisp
+++ src/midi.lisp
@@ -7,48 +7,65 @@
   (let ((k (- key 69)))
     (* 440 (expt twelveth-root-of-2 k))))
 
-(defparameter *keys* (make-hash-table))
+(defparameter *keys1* (make-hash-table))
+(defparameter *keys2* (make-hash-table))
 
-(defun get-tone (key)
-  (let ((tone (gethash key *keys*)))
+(defun get-tone (track key)
+  (let* ((keys (if (eq track 1) *keys1* *keys2*))
+         (tone (gethash key keys)))
     (unless tone
       (setf tone
-            (setf (gethash key *keys*)
+            (setf (gethash key keys)
                   (make-instance 'bell :freq  (key-to-freq key)))))
     tone))
 
 (defun wait-for-target-time (msg time-base)
   (let ((relative-time (- (get-internal-real-time) time-base))
         (target-time (midi:message-time msg)))
-    (format t "target ~a current ~a ~%" target-time relative-time)
-    (let ((delay (/ (- target-time relative-time) 1000)))
+    (let ((delay (/ (- target-time relative-time)
+                    INTERNAL-TIME-UNITS-PER-SECOND)))
       (when (> delay 0)
         (sleep delay)))))
 
-(defgeneric play-midi-msg (msg engine time-base)
-  (:method (msg engine time-base)
+(defgeneric play-midi-msg (msg track engine time-base)
+  (:method (msg track engine time-base)
     (format t "play ~a~%" msg))
-  (:method ((msg midi:note-on-message) engine time-base)
+  (:method ((msg midi:note-on-message) track engine time-base)
     (wait-for-target-time msg time-base)
     (format t "key on: ~a~%" (midi:message-key msg))
     (start-sound engine 
-                 (get-tone
+                 (get-tone track
                   (midi:message-key msg))))
-  (:method ((msg midi:note-off-message) engine time-base)
+  (:method ((msg midi:note-off-message) track engine time-base)
     (wait-for-target-time msg time-base)
     (format t "key off: ~a~%" (midi:message-key msg))
     (stop-sound engine 
-                (get-tone
+                (get-tone track
                  (midi:message-key msg)))))
+
+(defun flatten-tracks (tracks)
+  (let ((combined nil)
+        (tid 0))
+    (dolist (track tracks)
+      (dolist (msg track)
+        (when (and (or (= tid 1) (= tid 2))
+                   (or (eq (type-of msg) 'midi:note-on-message)
+                       (eq (type-of msg) 'midi:note-off-message)))
+          (setf combined (cons (list tid msg) combined))))
+      (incf tid))
+    (sort combined (lambda (a b)
+                     (< (midi:message-time (cadr a))
+                        (midi:message-time (cadr b)))))))
 
 (defun play ()
   (let* ((tracks (midi:midifile-tracks (midi:read-midi-file *midi-file-path*)))
          (engine (make-instance 'audio-engine))
-         (time-base (get-internal-real-time))
-         ;; (playing-tracks (list (car tracks) (cadr tracks)))
-         )
+         (time-base (get-internal-real-time)))
     (init engine)
     (unwind-protect
-         (loop for msg in (caddr tracks)
-               do (play-midi-msg msg engine time-base))
+         (loop for tid-msg in (flatten-tracks tracks)
+               do
+                  (let ((tid (car tid-msg))
+                        (msg (cadr tid-msg)))
+                    (play-midi-msg tid msg engine time-base)))
       (fini engine))))
